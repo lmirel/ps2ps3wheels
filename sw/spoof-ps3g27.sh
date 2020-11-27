337d0e32014656646bf9804e42813e6a354d9d85
mirel <mirel.lazar@gmail.com>
2020-11-27 11:03:03 +0100

    add spoof scripts

---
+++ sw/spoof-ps3g27.sh
@@ -0,0 +1,2 @@
+./usbxtract --tty /dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0 --device 0eb7:0e04 --spoof 046D:C29B
+#./usbxtract --tty /dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0 --device 046d:c260 --spoof 046D:C29B
